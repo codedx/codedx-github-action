6e2cc863c9e5d884e7c50221a5e918946517f9c3
Tyler Camp <tylerc@synopsys.com>
2021-09-23 16:15:40 -0400

    More detailed/accurate connection validation

--- codedx.js
+++ codedx.js
@@ -29,14 +29,35 @@ class CodeDxApiClient {
     }
 
     async testConnection() {
-        await this.anonymousHttp.get('/x/system-info')
+        const response = await this.anonymousHttp.get('/x/system-info')
+        if (typeof response.data != 'object') {
+            throw new Error(`Expected JSON Object response, got ${typeof response.data}. Is this a Code Dx instance?`)
+        }
+
+        const expectedFields = ['version', 'date']
+        const unexpectedFields = _.without(_.keys(response.data), ...expectedFields)
+        if (unexpectedFields.length > 0) {
+            throw new Error(`Received unexpected fields ${unexpectedFields.join(', ')}. Is this a Code Dx instance?`)
+        }
+
+        return response.data.version
     }
 
     async validatePermissions(projectId) {
         const neededPermissions = [
             `analysis:create:${projectId}`
         ]
-        const response = await this.http.post('/x/check-permissions', neededPermissions)
+
+        let response = null
+        try {
+            response = await this.http.post('/x/check-permissions', neededPermissions)
+        } catch (e) {
+            if (axios.isAxiosError(e) && e.response.status == 403) {
+                throw new Error("Permissions check responded with HTTP 403, is the API key valid?")
+            } else {
+                throw e
+            }
+        }
         const permissions = response.data
         const missingPermissions = neededPermissions.filter(p => !permissions[p])
         if (missingPermissions.length > 0) {
