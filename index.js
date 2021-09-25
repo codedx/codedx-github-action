019151127e7399b266060e9ea93f852e89f55169
Tyler Camp <tylerc@synopsys.com>
2021-09-25 12:36:50 -0400

    ... try actually running the `prepare` script

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 const core = require('@actions/core')
-// const analyze = require('./analyze')
+const analyze = require('./analyze')
 
-// async function main() {
-//     try {
-//         core.info("Starting from main!")
-//         await analyze()
-//     } catch (err) {
-//         core.info("Caught an error!")
-//         core.setFailed(err.message)
-//     }
-// }
-
-// main()
-
-core.info("This doesn't do anything!")
+analyze().catch(err => core.setFailed(err.message))
